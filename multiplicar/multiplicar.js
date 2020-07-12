bf7266ff89806248e84673dbba4f3eda60bacf99
Magin <magin1104@gmail.com>
2020-07-12 16:52:00 -0400

    Tabla de multiplicar con NodeJS

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,47 @@
+//require
+const fs = require('fs');
+const colors = require('colors');
+
+
+let listarTabla = (base, limite = 10)=>{
+
+    console.log('=========================='.green);
+    console.log(`Tabla del ${base}`.red);
+    console.log('=========================='.green);
+    for(let i=1; i<=limite; i++){
+        console.log(`${base} * ${i} = ${base*i}`);
+    }
+
+}
+
+let crearArchivo = (base, limite=10) =>{
+    return new Promise((resolve,reject) => {
+
+        if(!Number(base)){
+            //reject hace referencia al error
+            reject(`El valor introducido ${base} no es numero..!`);
+            return;
+        }
+        let data ='';
+
+        for(let i=1;i<=limite;i++){
+            data+=`${base} * ${i} = ${base*i}\n`;
+        }
+
+        fs.writeFile(`tablas/tabla_${base}.txt`, data, (err) => {
+        if (err) 
+            reject(err)
+        else
+            resolve(`tabla_${base}.txt`)
+
+        });
+    });
+
+}
+
+//LOS MODULOS NOS PERMITEN EXPORTAR LAS FUNCIONES QUE QUERRAMOS
+module.exports = {
+    crearArchivo,
+    listarTabla
+}
+
